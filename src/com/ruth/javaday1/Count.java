b59be8546c71314329e952bcc9ec4fa3910a0179
Carthen <rcarthen@teksystems.com>
2019-04-03 22:17:20 -0500

    update

--- src/com/ruth/javaday1/Count.java
+++ src/com/ruth/javaday1/Count.java
@@ -1,21 +1,17 @@
 package com.ruth.javaday1;
 
-import java.util.ArrayList;
+
 import java.util.Collections;
 import java.util.List;
 
 public class Count {
+	
 
-	public int isRepeat() {
+	public int isRepeat(String animal,List<String> animals) {
 
-		List<String> animals = new ArrayList<>();
-		animals.add("bat");
-		animals.add("owl");
-		animals.add("bat");
-		animals.add("bat");
-		System.out.println("Freq of bat: " + Collections.frequency(animals, "bat"));
-		return Collections.frequency(animals, "bat");
-
+		return Collections.frequency(animals, animal);
+		 
+		
 	}
 
 }
--- src/com/ruth/javaday1/Main.java
+++ src/com/ruth/javaday1/Main.java
@@ -1,18 +1,35 @@
 package com.ruth.javaday1;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		  List<Integer> arrlist = new ArrayList<>();
+		    arrlist.add(1);
+		    arrlist.add(2);
+		    arrlist.add(3);
+		    arrlist.add(4);
+		    arrlist.add(5);
+		    arrlist.add(6);
+		    
+		    MyList  mylist= new MyList();
+		    
 	
-		
-		MyList numberlist = new MyList();
-		numberlist.isListEmpty();
-		
-		Count animals= new Count();
-		animals.isRepeat();
-		
-		
+		    System.out.println("The middle element is " + mylist.isMiddleElement(arrlist));
+		    
+		    
+		    List<String> animals = new ArrayList<>();
+			animals.add("bat");
+			animals.add("owl");
+			animals.add("bat");
+			animals.add("bat");
+			
+			Count mycount= new Count();
+			System.out.println("The frequency of bat is " + mycount.isRepeat("bat", animals));
+			
 		
 	}
 	
